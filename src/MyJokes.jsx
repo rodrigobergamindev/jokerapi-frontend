bc9b56cc012fc1b5886a1e5bcfe32b4c33def59f
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 16:57:09 -0300

    .

--- src/MyJokes.jsx
+++ src/MyJokes.jsx
@@ -13,88 +13,93 @@ import {useParams} from 'react-router-dom'
 
 export default function MyJokes() {
     const [jokes, setJokes] = useState([])
-    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(false)
     let {slug} = useParams()
 
     useEffect(() => {
         
-        try {
+        
             async function fetchData() {
-                const response = await axios.get(`https://jokerapi-usjt.herokuapp.com/user/${slug}/jokes`, {
-                   withCredentials: true
-                })
+                try {
+              
+                    const response = await axios.get(`https://jokerapi-usjt.herokuapp.com/user/${slug}/jokes`, {
+                       withCredentials: true
+                    })
+    
+                    setJokes(response.data)
                 
-                console.log(response.status)
-
-                setJokes(response.data)
+            } catch (error) {
+                 
+                  if(error.response.status === 401){
+                      setErrorMessage(true)
+                  }
+                
+                
             }
-    
-            fetchData()
-        } catch (error) {
-            setError(true)
-        }
-    },[])
+            }
 
-    
+            fetchData()
+        },[])
+
+  
 
   return (
 
       <>
       
-      {
-          !!error? (
-            <VStack width="100%" height="100%" spacing={8}>
-            <HStack 
-            width="100%" 
-            backgroundColor="blue.700"  
-            alignItems="center" 
-            justifyContent="center" 
-            textAlign="center" 
-            borderBottom="1px" 
-            borderColor="gray.500"
-            padding="10px"
-            >
-                <Text fontSize="3xl" fontWeight="black" color="whiteAlpha.900">Joker API</Text>
-            </HStack>
-            <Heading>{`Jokes for ${slug}`}</Heading>
-              <Grid maxWidth="1200px" width="100%" templateColumns='repeat(4, 1fr)' justifyItems="center" gap={6}>
-                    {
-                        jokes.map(joke => (
-                            <VStack key={joke.id} boxShadow="md" w="350px" h="250px" backgroundColor="tomato" padding="10px">
-                                <HStack alignSelf="flex-start" justifyContent="space-between" borderBottom="2px" borderColor="whiteAlpha.900" width="100%" paddingBottom="2">
-                                <Text color="white" fontWeight="bold">{joke.category}</Text>
-                                <Text fontWeight="bold" color="white">{new Date(joke.createdAt).toLocaleDateString('pt-BR',{
-                                    dateStyle: "short"
-                                })}</Text>
-                                </HStack>
-                                <Text fontSize="md" fontWeight="bold">{joke.title}</Text>
-                                <Text textAlign="justify" overflowY="scroll" css={{
-                                    '&::-webkit-scrollbar': {
-                                        width: '4px',
-                                      },
-                                      '&::-webkit-scrollbar-track': {
-                                        width: '6px',
-                                      },
-                                      '&::-webkit-scrollbar-thumb': {
-                                        background: '#ecf0f1',
-                                        borderRadius: '24px',
-                                      },
-                                }} >{joke.description}</Text>
-                                <HStack alignSelf="flex-end" spacing={2}>
-                                <Text>Author: @{joke.author.username}</Text>
-                                </HStack>
-                            </VStack>
-                        ))
-                    }
-           
-              </Grid>
-          </VStack>
-          ) : (
-              <VStack>
-                  <Text>Ocorreu um erro</Text>
+      
+           {!errorMessage ? (
+                <VStack width="100%" height="100%" spacing={8}>
+                <HStack 
+                width="100%" 
+                backgroundColor="blue.700"  
+                alignItems="center" 
+                justifyContent="center" 
+                textAlign="center" 
+                borderBottom="1px" 
+                borderColor="gray.500"
+                padding="10px"
+                >
+                    <Text fontSize="3xl" fontWeight="black" color="whiteAlpha.900">Joker API</Text>
+                </HStack>
+                <Heading>{`Jokes for ${slug}`}</Heading>
+                  <Grid maxWidth="1200px" width="100%" templateColumns='repeat(4, 1fr)' justifyItems="center" gap={6}>
+                        {
+                            jokes.map(joke => (
+                                <VStack key={joke.id} boxShadow="md" w="350px" h="250px" backgroundColor="tomato" padding="10px">
+                                    <HStack alignSelf="flex-start" justifyContent="space-between" borderBottom="2px" borderColor="whiteAlpha.900" width="100%" paddingBottom="2">
+                                    <Text color="white" fontWeight="bold">{joke.category}</Text>
+                                    <Text fontWeight="bold" color="white">{new Date(joke.createdAt).toLocaleDateString('pt-BR',{
+                                        dateStyle: "short"
+                                    })}</Text>
+                                    </HStack>
+                                    <Text fontSize="md" fontWeight="bold">{joke.title}</Text>
+                                    <Text textAlign="justify" overflowY="scroll" css={{
+                                        '&::-webkit-scrollbar': {
+                                            width: '4px',
+                                          },
+                                          '&::-webkit-scrollbar-track': {
+                                            width: '6px',
+                                          },
+                                          '&::-webkit-scrollbar-thumb': {
+                                            background: '#ecf0f1',
+                                            borderRadius: '24px',
+                                          },
+                                    }} >{joke.description}</Text>
+                                    <HStack alignSelf="flex-end" spacing={2}>
+                                    <Text>Author: @{joke.author.username}</Text>
+                                    </HStack>
+                                </VStack>
+                            ))
+                        }
+               
+                  </Grid>
               </VStack>
-          )
-      }
+           ) : (
+               <Text>Ocorreu um erro</Text>
+           )}
+          
+      
       </>
    
   )
