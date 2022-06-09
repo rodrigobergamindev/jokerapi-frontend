93569b81d81c41218bf4e307e5a81531ebd56f53
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-06-09 14:11:43 -0300

    .

---
+++ src/Register.jsx
@@ -0,0 +1,107 @@
+import React, {useState, useEffect} from 'react'
+import {
+    Box,
+    Text,
+    VStack,
+    HStack,
+    Heading,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Button,
+    
+  } from '@chakra-ui/react';
+import axios from 'axios'
+import {Formik, Field, Form} from 'formik'
+
+export default function Register() {
+    
+    function validate(value) {
+        let error
+        if (!value) {
+          error = 'This field is required'
+        } 
+        return error
+      }
+
+  return (
+
+      <VStack width="100%" height="100vh" alignItems="center" justifyContent="center" spacing={8}>
+       
+        <Heading>Register</Heading>
+          
+        <VStack>
+        <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={(values, actions) => {
+        const {username, password} = values
+       setTimeout(async () => {
+        try {
+            const sendValues = await axios.post('https://jokerapi-usjt.herokuapp.com/user', {
+            username,
+            password
+        },
+        {
+            withCredentials: true
+        })
+    
+        if(sendValues.data.token){
+           //redirect
+        }
+
+       
+        actions.setSubmitting(false)
+        } catch (error) {
+            console.log(error)
+            if(error.response.status === 409){
+                alert('Usuário já existe')
+            }
+            actions.setSubmitting(false)
+        }
+      }, 1000)
+      }}
+
+    >
+      {(props) => (
+        <Form>
+          <Field name='username' validate={validate}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.username && form.touched.username}>
+                <FormLabel htmlFor='username'>Username</FormLabel>
+                <Input {...field} id='username' placeholder='username' />
+                <FormErrorMessage>{form.errors.username}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+
+            
+          <Field name='password' validate={validate}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.password && form.touched.password}>
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <Input {...field} id='password' placeholder='password' />
+                <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+
+
+          <Button
+            mt={4}
+            colorScheme='blue'
+            isLoading={props.isSubmitting}
+            type='submit'
+          >
+            Sign Up
+          </Button>
+        </Form>
+      )}
+    </Formik>
+        </VStack>
+
+      </VStack>
+   
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import Jokes from './Jokes';
 import { ChakraProvider, theme } from '@chakra-ui/react';
+import Register from './Register';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <>
   <ChakraProvider theme={theme}>
-    <Jokes/>
+    <Register/>
   </ChakraProvider>
   </>
 );
