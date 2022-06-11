91c55fb8825864e3d9a3f0dd843f69c93c32ea85
rodrigobergamindev <rodrigobergamindev@gmail.com>
2022-06-11 03:53:03 -0300

    .

--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -44,34 +44,34 @@ export default function Jokes() {
         </HStack>
         <Heading>All Jokes</Heading>
           <Grid maxWidth="1200px" width="100%" templateColumns='repeat(4, 1fr)' justifyItems="center" gap={6}>
-                {
-                    jokes.map(joke => (
-                        <VStack key={joke.id} boxShadow="md" w="350px" h="250px" backgroundColor="tomato" padding="10px">
-                            <HStack alignSelf="flex-start" justifyContent="space-between" borderBottom="2px" borderColor="whiteAlpha.900" width="100%" paddingBottom="2">
-                            <Text color="white" fontWeight="bold">{joke.category}</Text>
-                            <Text fontWeight="bold" color="white">{new Date(joke.createdAt).toLocaleDateString('pt-BR',{
-                                dateStyle: "short"
-                            })}</Text>
-                            </HStack>
-                            <Text fontSize="md" fontWeight="bold">{joke.title}</Text>
-                            <Text textAlign="justify" overflowY="scroll" css={{
-                                '&::-webkit-scrollbar': {
-                                    width: '4px',
-                                  },
-                                  '&::-webkit-scrollbar-track': {
-                                    width: '6px',
-                                  },
-                                  '&::-webkit-scrollbar-thumb': {
-                                    background: '#ecf0f1',
-                                    borderRadius: '24px',
-                                  },
-                            }} >{joke.description}</Text>
-                            <HStack alignSelf="flex-end" spacing={2}>
-                            <Text>Author: @{joke.author.username}</Text>
-                            </HStack>
-                        </VStack>
-                    ))
-                }
+          {
+                            jokes.map(joke => (
+                                <VStack key={joke.id} boxShadow="md" w="350px" h="250px" backgroundColor="gray.400" padding="10px">
+                                    <HStack alignSelf="flex-start" justifyContent="space-between" borderBottom="2px" borderColor="whiteAlpha.900" width="100%" paddingBottom="2">
+                                    <Text color="white" fontWeight="bold">{joke.category}</Text>
+                                    <Text fontWeight="bold" color="white">{new Date(joke.createdAt).toLocaleDateString('pt-BR',{
+                                        dateStyle: "short"
+                                    })}</Text>
+                                    </HStack>
+                                    <Text fontSize="md" fontWeight="bold">{joke.title}</Text>
+                                    <Text textAlign="justify" maxW="100%" height="100%" overflowY="scroll" css={{
+                                        '&::-webkit-scrollbar': {
+                                            width: '4px',
+                                          },
+                                          '&::-webkit-scrollbar-track': {
+                                            width: '6px',
+                                          },
+                                          '&::-webkit-scrollbar-thumb': {
+                                            background: '#ecf0f1',
+                                            borderRadius: '24px',
+                                          },
+                                    }} >{joke.description}</Text>
+                                    <HStack alignSelf="flex-end" spacing={2}>
+                                    <Text>Author: @{joke.author.username}</Text>
+                                    </HStack>
+                                </VStack>
+                            ))
+                        }
        
           </Grid>
       </VStack>
